38ce00af1f747d3a8704804681729458b7b544ae
huang <31849938+15332005@users.noreply.github.com>
2018-07-07 23:08:22 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  path: '/mine',
-  indexRoute:{
-    getComponent (location, callback) {
-      import('../../pages/Mine').then(module => callback(null, module.default));
-    }
-  }
-}
