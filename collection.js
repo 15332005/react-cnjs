c382420bb68c5134ba122f88b188b478be462178
huang <31849938+15332005@users.noreply.github.com>
2018-07-07 23:04:18 +0800

    Delete collection.js

--- collection.js
+++
@@ -1,6 +0,0 @@
-export default {
-  path: '/mine/collection',
-  getComponent (location, callback) {
-    import('../../pages/Mine/Collection').then(module => callback(null, module.default));
-  }
-}
